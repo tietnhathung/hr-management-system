41a693488eac9f02458fb65c49b6c8090eef63b5
tietnhathung <tietnhathung@gmail.com>
2021-04-27 23:11:10 +0700

    them class face

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,22 @@
 from configparser import ConfigParser
+from src.face import Face
+import cv2
 
 config = ConfigParser()
-
 config.read('config.ini')
+
+face = Face()
+
+image = cv2.imread('./images/test1.jpg')
+
+face.set_image(image)
+
+list_vec_faces = face.Face_vec()
+
+print(list_vec_faces)
+
+
+
+
+
 
---
+++ backend/src/face.py
@@ -0,0 +1,61 @@
+from configparser import ConfigParser
+import cv2
+import imutils
+import os
+import numpy as np
+
+config = ConfigParser()
+config.read('config.ini')
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+class Face:
+    caffeDetector = None
+    torchEmbedder = None
+
+    def __init__(self):
+        protoCaffePath = os.path.join(ROOT_DIR, config['DEFAULT']['ModelDir'] , 'deploy.prototxt')
+        modelCaffePath = os.path.join(ROOT_DIR, config['DEFAULT']['ModelDir'] , 'res10_300x300_ssd_iter_140000.caffemodel')
+        self.caffeDetector = cv2.dnn.readNetFromCaffe(protoCaffePath, modelCaffePath)
+        modelTorchPath = os.path.join(ROOT_DIR, config['DEFAULT']['ModelDir'] , 'openface_nn4.small2.v1.t7')
+        self.torchEmbedder = cv2.dnn.readNetFromTorch(modelTorchPath)
+
+    def Recognition(self,image):
+        list_poit_faces = []
+
+        (height, width) = image.shape[:2]
+
+        imageBlob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0),swapRB=False, crop=False)
+        self.caffeDetector.setInput(imageBlob)
+        detections = self.caffeDetector.forward()
+
+        list_faces = detections[0, 0]
+
+        filter = np.greater(list_faces[:,2], float(config['FACE']['Confidence']) )
+
+        list_poit_faces = list_faces[filter, 2:7] * np.array([1,width, height, width, height])
+
+        return list_poit_faces
+
+    def Face_vec(self,image,box):
+        list_vec_faces = []
+        list_poit_faces = box
+        for poit_face in list_poit_faces:
+            (startX, startY, endX, endY) = poit_face.astype("int")
+
+            face_image = image[startY:endY, startX:endX]
+
+            (fH, fW) = face_image.shape[:2]
+
+            if fW < 20 or fH < 20:
+                continue
+
+            faceBlob = cv2.dnn.blobFromImage(face_image, 1.0 / 255, (96, 96), (0, 0, 0), swapRB=True, crop=False)
+            self.torchEmbedder.setInput(faceBlob)
+            vec = self.torchEmbedder.forward()
+            list_vec_faces.append(vec)
+
+        return list_vec_faces
+
+
+
